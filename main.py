47c6efc1730061bc48ca7f12d4a8c61e5a4bb0f3
bieltv3 <bieltv.3@gmail.com>
2018-10-16 11:05:55 +0200

    Main program and requiriments

---
+++ main.py
@@ -0,0 +1,23 @@
+# System dependencies
+import sys
+
+# Packages import
+from text2classification import classify
+from speech2text import local, cloud
+
+# Check if there is new wav file in the folder
+
+# Pre-process the audio file for a better understanding
+
+# Try local-based speech2text
+text = local.transcribe(sys.argv[1])
+print("LOCAL: " + text)
+
+# If sentence is not understandable - cloud speech2text
+text = cloud.transcribe(sys.argv[1])
+print("CLOUD: " + text)
+
+# Classify the words with text2classification
+classify.classify_sentence(text)
+
+# Output
