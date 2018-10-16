17153e2d42e0285c11426dbcd1e39e144a27ad23
bieltv3 <bieltv.3@gmail.com>
2018-10-16 11:04:16 +0200

    Add speech2text module

---
+++ speech2text/cloud.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+
+def transcribe(AUDIO_FILE):
+	
+	# Read the audio file 	
+	r = sr.Recognizer()
+	with sr.AudioFile(AUDIO_FILE) as source:
+		audio = r.record(source)
+
+	try:
+		return r.recognize_google(audio)
+	except sr.UnknownValueError:
+		return -1
+	except sr.RequestError as e:
+		return -1
